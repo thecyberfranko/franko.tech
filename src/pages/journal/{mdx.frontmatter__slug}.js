a5a549c4fd7efb54490f417261801f902e5f2c55
Franko <micha3lmf@gmail.com>
2024-04-23 13:00:35 -0600

    Finished Part 7

--- src/pages/journal/{mdx.frontmatter__slug}.js
+++ src/pages/journal/{mdx.frontmatter__slug}.js
@@ -2,11 +2,17 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPost = ({ data, children }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.title}</p>
+            <p>LOGS: {data.mdx.frontmatter.title}</p>
+            <GatsbyImage
+                image={ image }
+                alt={ data.mdx.frontmatter.hero_image_alt }
+            />
             {children}
         </Layout>
     )
@@ -18,6 +24,12 @@ export const query = graphql`
             frontmatter {
                 title
                 date(formatString: "MMM D, YYYY")
+                hero_image_alt
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
         }
     }
